c59754c12a19f0dad46a558d0f7c9d219e2798ac
aleksmagnusson <aleks.magnusson@gmail.com>
2023-01-04 22:20:46 +0100

    All 3 maps with the project exams -- done --

---
+++ CopyCat_plugin/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <navbar>
+            <div className="navbar">
+                <div className="navbar-1">
+                    <img className="menu" src={menu} />
+                    <span className="menu-1">MENU</span>
+                    <img className="search" src={search} />
+                    <span className="search-1">SEARCH</span>
+                    <img className="frame-11" src={frame11} />
+                </div>
+            </div>
+        </navbar>
+    )
+}
+
+export default Navbar
