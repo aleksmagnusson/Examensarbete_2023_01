af2e3b7b0f8e0138a3674b4a743285e02439ac22
aleksmagnusson <aleks.magnusson@gmail.com>
2023-01-10 13:46:00 +0100

    ownBuild soon components done

---
+++ ownBuild/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <nav>
+                Navbar
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
