7ade0de36fe6021bcb7e3ffbf00aa42c24ba9368
Aleks Magnusson <92426232+aleksmagnusson@users.noreply.github.com>
2023-01-11 16:27:46 +0100

    Finnish Off Work

--- ownBuild/src/components/LP.jsx
+++ ownBuild/src/components/LP.jsx
@@ -22,19 +22,25 @@ function LP() {
                 </div>
                 <div>
                     <box className='boxContainer1'>
-                        <h2>Image 1</h2>
-                        <h2>Image 2</h2>
+                        <img src="https://images.unsplash.com/photo-1583912267670-6575ad472688?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=535&q=80" height="214" width="535" alt='women do science work' />
+                        <img src='https://images.unsplash.com/photo-1492496913980-501348b61469?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=535&q=80' height="214" width="535" alt='hands holding dirt' />
                     </box>
                     <div>
-                        <button>SKIN CELL SCIENCE</button>
-                        <button>BEHIND THE HANDCRAFTS</button>
+                        <button className='learnMore1'>SKIN CELL SCIENCE</button>
+                        <button className='learnMore1'>BEHIND THE HANDCRAFTS</button>
                     </div>
                 </div>
             </container>
             <container className="container3">
+                <img src="PictureSection.png" height="400" width="610" />
+                <box className="boxContainer2">
+                    <h2>LATEST FROM CPB:</h2>
+                    <h4>WE HAVE PLACES AVAILABLE RIGHT NOW FOR CLÈ DE PEAU BEAUTÈ's E-LEARNING.
+                        RESERVE YOUR PLACE NOW! LEARN TOGETHER ABOUT OUR SUSTAINABILITY</h4>
+                    <button className='learnMore1'>LEARN MORE</button>
 
+                </box>
             </container>
-
         </div>
     )
 }
